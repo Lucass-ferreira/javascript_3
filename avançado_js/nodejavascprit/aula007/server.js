cefba0e1d9862d2b03f6f7599a9c979cb9108e9e
Lucas Ferreira <150602557+Lucass-ferreira@users.noreply.github.com>
2026-08-21 08:06:04 -0300

    instalando o nodemon e utilizando

---
+++ avançado_js/nodejavascprit/aula007/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    // res.send('Hello <b>word!</b>');
+    res.send(`
+        <form action="/" method="POST">
+            Nome: <input type="text" name="nome">
+            <button>Enviar tudo</button>
+        </form>    
+    `)
+});
+
+app.post('/', (req, res) => {
+    res.send('Recebi o formulario')
+})
+
+app.get('/contato' , (req, res) => {
+    res.send('Obrigado por entra em contato com a gente')
+})
+
+app.listen(3000, () => {
+    console.log('Acessar http://localhost:3000')
+    console.log('Servidor executando na porta 3000')
+})
